0b4b641ab9793ba03500a947b82e9559ef37d332
Wajida Gouhar Shad <baigwajida@gmail.com>
2023-10-01 09:53:59 +0000

    minor fix in main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ module "terrahouse_aws" {
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
-git   content_version = var.content_version
+  content_version = var.content_version
 }
 
