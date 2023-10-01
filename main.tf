f1ad32f116381d4b883b80fef704d9a6337833ee
Wajida Gouhar Shad <baigwajida@gmail.com>
2023-10-01 13:55:41 +0000

    fixing code traps

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ module "terrahouse_aws" {
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
   content_version = var.content_version
 }
 
