c6fa9c738d32979a63f432e6294c069f9ecb32ad
Wajida Gouhar Shad <baigwajida@gmail.com>
2023-10-03 13:07:59 +0000

    to destroy infra finally

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
     key = "error.html"
   }
 }
-
+/*
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
@@ -42,7 +42,7 @@ resource "aws_s3_object" "error_html" {
 
   etag = filemd5("${path.root}${var.error_html_filepath}")
 }
-
+*/
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
   #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
