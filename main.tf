7875d5fd5eb26df2591fd44b52f8eb86da22360f
Wajida Gouhar Shad <147150822+baigwajida-gs@users.noreply.github.com>
2023-10-13 19:12:53 +0530

    54 resource skeleton (#56)
    * #54 setup skeleton for resources
    
    * #54 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -38,4 +38,18 @@ provider "terratowns" {
 #   content_version = var.content_version
 # }
 
-
+resource "terratowns_home" "home" {
+  name = "How to make Kunafa/Knafeh!!!"
+  description = <<DESCRIPTION
+Kunafa or Knafeh is a traditional Middle Eastern dessert, 
+made with spun pastry called kataifi,
+soaked in a sweet, sugar-based syrup called attar, 
+and typically layered with cheese, 
+or with other ingredients such as clotted cream, 
+pistachio or nuts, depending on the region.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "4wx3yz2ab1cd.cloudfront.net"
+  town = "cooker-cove"
+  content_version = 1
+}
