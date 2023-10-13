51cecc5ea71294bbb5c8354a7483b614bb4acc90
Wajida Gouhar Shad <147150822+baigwajida-gs@users.noreply.github.com>
2023-10-14 00:02:08 +0530

    #57 test our terra home on terratowns test town (#58)

--- main.tf
+++ main.tf
@@ -22,24 +22,23 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   assets_path = var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home" {
-  name = "How to make Kunafa/Knafeh!!!"
+  name = "How to make Kunafa/Knafeh!"
   description = <<DESCRIPTION
 Kunafa or Knafeh is a traditional Middle Eastern dessert, 
 made with spun pastry called kataifi,
@@ -48,8 +47,7 @@ and typically layered with cheese,
 or with other ingredients such as clotted cream, 
 pistachio or nuts, depending on the region.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "4wx3yz2ab1cd.cloudfront.net"
-  town = "cooker-cove"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
