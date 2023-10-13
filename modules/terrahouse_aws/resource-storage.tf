5b0907700cc172bc4d1d2eeeec6dce7df19215e1
Wajida Gouhar Shad <baigwajida.gs@gmail.com>
2023-10-13 18:30:59 +0000

    #57 test our terra home on terratowns test town

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -2,7 +2,8 @@
 resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  bucket = var.bucket_name
+  # we want to assign a random bucket name
+  #bucket = var.bucket_name
 
   tags = {
     UserUuid = var.user_uuid
@@ -42,7 +43,7 @@ resource "aws_s3_object" "upload_assets" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
   source = "${var.assets_path}/${each.key}"
-  etag = filemd5("${var.assets_path}/${each.key}")
+  etag = filemd5("${var.assets_path}${each.key}")
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
     ignore_changes = [etag]
